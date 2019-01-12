00d2efd1c8ab3bdf3dd29b8b4607f867d26feebf
LamerVano <vania_piskunov@tut.by>
2019-01-12 21:10:48 +0300

    Del All Unnecessary usings

--- InternetShop/Common/User.cs
+++ InternetShop/Common/User.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Common
 {
-    
+
     public class User: IModel
     {
         public int UserId { get; set; }
